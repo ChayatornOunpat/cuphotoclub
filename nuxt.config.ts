14804bb140dfb740f6314f30bcd4a86742ae5db3
ChayatornOunpat <newmainmailelectronic@gmail.com>
2026-06-21 00:40:04 +0700

    chore:add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxthub/core',
+    'nuxt-auth-utils'
+  ]
 })
